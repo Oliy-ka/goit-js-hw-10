8769a971c48e8a4508c99ab111e9791abe174117
oliyka <olja.x.06@gmail.com>
2025-06-05 22:40:21 +0300

    added

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,3 +2,43 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+const button = document.querySelector("button");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const formSubmit = event.currentTarget;
+    const delay = parseInt(form.elements.delay.value);
+    const state = form.elements.state.value;
+
+    const promise = new Promise((resolve, reject) => { 
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise.then(delay => {
+        console.log(`✅ Fulfilled promise in ${delay}ms`);
+        iziToast.success({
+            title: 'OK',
+            message: `✅ Fulfilled promise in ${delay}ms`,
+        });
+        
+      })
+      .catch(delay => {
+          console.log(`❌ Rejected promise in ${delay}ms`);
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+            });
+      });
+    
+});
+
+
+
